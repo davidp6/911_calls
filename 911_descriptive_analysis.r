cd7f86808b0b0677600d15518a07c8849c918262
David Vogt Phillips <davidemphillips@gmail.com>
2018-05-31 22:30:40 -0700

    Make the descriptive analysis generic to cities

--- 911_descriptive_analysis.r
+++ 911_descriptive_analysis.r
@@ -39,30 +39,17 @@ graphFile = './descriptive_analysis.pdf'
 # Load/prep data
 
 # load all files
-dataSea = fread(inFileSea)
-dataBal = fread(inFileBal) # doesn't have event classifications
-dataLA = fread(inFileLA) # doesn't have event classifications
-dataDet = fread(inFileDet)  # doesn't have event classifications
-
-# rename
-setnames(dataSea, c('record_id', 'event_number', 'general_offense_number',
-				'clearance_code', 'clearance_description', 'clearance_subgroup',
-				'clearance_group', 'call_date_time', 'address', 'district',
-				'zone', 'census_tract', 'longitude', 'latitude', 'location',
-				'initial_type_description', 'initial_type_subgroup',
-				'initial_type_group', 'at_scene_time'))
-# setnames(dataBal, c('record_id', 'call_date_time', 'priority', 'district',
-# 				'initial_type_description', 'event_number', 'address', 'location'))
-
-# append
-
+data = readRDS(inFile)
 
 # missing values
-data[Initial_Type_Group=='', Initial_Type_Group:='NOT RECORDED']
+data[initial_type_group=='', initial_type_group:='NOT RECORDED']
+
+# remove calls with no description
+data[initial_type_description=='', initial_type_group_predicted:='NO DESCRIPTION']
 
 # date formats
-data[, c('date_str', 'time_str', 'AMPM'):=tstrsplit(At_Scene_Time, ' ', fixed=TRUE)]
-data[is.na(date_str), c('date_str', 'time_str', 'AMPM'):=tstrsplit(Event_Clearance_Date, ' ', fixed=TRUE)]
+data[, c('date_str', 'time_str', 'AMPM'):=tstrsplit(at_scene_time, ' ', fixed=TRUE)]
+data[is.na(date_str), c('date_str', 'time_str', 'AMPM'):=tstrsplit(call_date_time, ' ', fixed=TRUE)]
 data[, call_date:=as.Date(date_str, '%m/%d/%Y')]
 data[, call_month:=month(call_date)]
 data[, call_year:=year(call_date)]
@@ -75,15 +62,15 @@ clear_danger = c('CRISIS CALL' ,'CASUALTIES', 'ASSAULTS', 'PERSONS - LOST, FOUND
 possible_danger = c('TRESPASS', 'RESIDENTIAL BURGLARIES', 'ANIMAL COMPLAINTS', 'ROBBERY',
 					'RECKLESS BURNING', 'ROAD RAGE')
 
-data[Initial_Type_Group %in% clear_danger, Initial_Type_Danger:='Clear Danger']
-data[Initial_Type_Group %in% possible_danger, Initial_Type_Danger:='Possible Danger']
-data[!(Initial_Type_Group %in% clear_danger) & !(Initial_Type_Group %in% possible_danger),
-					Initial_Type_Danger:='No Immediate Danger']
-data[, Initial_Type_Danger:=factor(Initial_Type_Danger,
+data[initial_type_group_predicted %in% clear_danger, initial_type_danger:='Clear Danger']
+data[initial_type_group_predicted %in% possible_danger, initial_type_danger:='Possible Danger']
+data[!(initial_type_group_predicted %in% clear_danger) & !(initial_type_group_predicted %in% possible_danger),
+					initial_type_danger:='No Immediate Danger']
+data[, initial_type_danger:=factor(initial_type_danger,
 		levels=c('Clear Danger',  'Possible Danger', 'No Immediate Danger'), ordered=TRUE)]
 
 # convert to title case (warning: slow)
-# data[, Initial_Type_Group:=toTitleCase(Initial_Type_Group)]
+# data[, initial_type_group_predicted:=toTitleCase(initial_type_group_predicted)]
 # ---------------------------------------------------------------
 
 
@@ -91,9 +78,10 @@ data[, Initial_Type_Danger:=factor(Initial_Type_Danger,
 # Descriptive Graphs
 
 # 911 cals by initial group
-tmpAgg = data[, .N, by=c('Initial_Type_Danger', 'Initial_Type_Group')]
-p1 = ggplot(tmpAgg, aes(y=N, x=reorder(Initial_Type_Group, -N), fill=Initial_Type_Danger)) +
+tmpAgg = data[, .N, by=c('initial_type_danger', 'initial_type_group_predicted', 'city')]
+p1 = ggplot(tmpAgg, aes(y=N, x=reorder(initial_type_group_predicted, -N), fill=initial_type_danger)) +
 	geom_bar(stat='identity') +
+	facet_wrap(~city, ncol=1) +
 	labs(title='Initial 911 Call Category', y='Frequency', x='') +
 	scale_fill_manual('', values=brewer.pal(3, 'Set1')) +
 	theme_bw() +
@@ -116,15 +104,15 @@ p3 = ggplot(tmpAgg, aes(y=N, x=call_my)) +
 	theme(axis.text.x=element_text(angle=315, hjust=0), plot.title=element_text(hjust=.5))
 
 # 911 calls by initial group over time
-tmpAgg = data[, .N, by=c('call_my', 'Initial_Type_Danger')]
-p4 = ggplot(tmpAgg, aes(y=N, x=call_my, color=Initial_Type_Danger)) +
+tmpAgg = data[, .N, by=c('call_my', 'initial_type_danger')]
+p4 = ggplot(tmpAgg, aes(y=N, x=call_my, color=initial_type_danger)) +
 	geom_line() +
 	labs(title='911 Calls per Month', y='Frequency', x='') +
 	scale_color_manual('', values=brewer.pal(3, 'Set1')) +
 	theme_bw() +
 	theme(axis.text.x=element_text(angle=315, hjust=0), plot.title=element_text(hjust=.5))
 tmpAgg[, pct:=N/sum(N), by='call_my']
-p5 = ggplot(tmpAgg, aes(y=pct*100, x=call_my, fill=Initial_Type_Danger)) +
+p5 = ggplot(tmpAgg, aes(y=pct*100, x=call_my, fill=initial_type_danger)) +
 	geom_bar(stat='identity', position='stack') +
 	labs(title='911 Calls per Month', y='Percentage', x='') +
 	scale_fill_manual('', values=brewer.pal(3, 'Set1')) +
@@ -142,18 +130,6 @@ p5 = ggplot(tmpAgg, aes(y=pct*100, x=call_my, fill=Initial_Type_Danger)) +
 # time between call time and at-scene time by initial group
 
 # ------------------------------------------------------------------------------------------
-
-
-# ---------------------------------
-# Run analysis
-
-# ---------------------------------
-
-
-# ----------------------------------------------
-# Analysis Graphs
-
-# ----------------------------------------------
 
 
 # --------------------------------
