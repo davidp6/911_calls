4ee9e46cc119e250184b88f3c31c755246be1732
David Vogt Phillips <davidemphillips@gmail.com>
2018-06-11 22:22:16 -0700

    Add csv output to explore_classification

--- Code/explore_classification.r
+++ Code/explore_classification.r
@@ -17,6 +17,7 @@
 # Set up R
 rm(list=ls())
 library(data.table)
+library(rowr)
 library(ggplot2)
 library(grid)
 library(gridExtra)
@@ -24,25 +25,47 @@ library(gtable)
 # --------------------
 
 
-# ----------------------------------------------
+# ---------------------------------------------------------------------
 # Files and directories
 
 # input files
 inFile = './Data/Cleaned Data/classified_data.rds'
 
-# output files
+# an easy-viewing pdf of the top descriptions per class
 graphFile = './Output/Model Diagnostics/classificiation_exploration.pdf'
-# ----------------------------------------------
+
+# a spreadsheet of the top descriptions per class
+outFile = './Output/Model Diagnostics/top_descriptions_per_class.csv'
+# ---------------------------------------------------------------------
 
 
-# ---------------------
+# ----------------------------------------------------------------------
 # Load data
 data = readRDS(inFile)
-# ---------------------
+
+# make frequency table
+freq = data[, .N, by=
+	c('initial_type_group_predicted', 'initial_type_description', 'city')]
+
+# reshape for better reading
+i=1
+for(g in unique(freq$initial_type_group_predicted)) {
+	tmp = freq[initial_type_group_predicted==g]
+	tmp = tmp[,c('city','initial_type_description','N'),with=FALSE]
+	setnames(tmp, c('City', paste(g,'Description'),'Frequency'))
+	tmp = tmp[order(City, -Frequency)]
+	if (i==1) outTable = tmp
+	if (i>1) outTable = cbind.fill(outTable, tmp, fill=NA)
+	i=i+1
+}
+# ----------------------------------------------------------------------
 
 
 # -----------------------------------------------------------
 # Manually assess classification for reasonableness
+
+# save a table
+write.csv(outTable, outFile)
 
 # open pdf
 pdf(graphFile, height=6, width=9)
